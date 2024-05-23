494461b56ea24491de90c9025dddfe2c4f02f7c4
mario <mario@localhost>
2024-05-23 06:16:44 +0200

    Add aditional tests for repo

--- src/test/java/hr/fer/eventstore/base/jpa/EventJpaRepositoryTest.java
+++ src/test/java/hr/fer/eventstore/base/jpa/EventJpaRepositoryTest.java
@@ -3,6 +3,7 @@ package hr.fer.eventstore.base.jpa;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
@@ -44,6 +45,63 @@ class EventJpaRepositoryTest extends TestContainersDbFixture {
     assertThat(loadedEvent.getVersion()).isEqualTo(1);
     assertThat(loadedEvent.getEventType()).isEqualTo("string");
     assertThat(loadedEvent.getEventTypeVersion()).isEqualTo(1);
+  }
+
+  @Test
+  void countByStreamId() throws Exception {
+    repo.saveAllAndFlush(List.of(
+        createStubEventWithStreamId("sid1", 1),
+        createStubEventWithStreamId("sid1", 2),
+        createStubEventWithStreamId("sid2", 1)
+    ));
+
+
+    assertThat(repo.countByStreamId("sid1")).isEqualTo(2);
+    assertThat(repo.countByStreamId("sid2")).isEqualTo(1);
+  }
+
+  @Test
+  void findEventsByStreamId() throws Exception {
+    repo.saveAllAndFlush(List.of(
+        createStubEventWithStreamIdAndData("sid1", 1, "\"d1\""),
+        createStubEventWithStreamIdAndData("sid1", 2, "\"d2\""),
+        createStubEventWithStreamIdAndData("sid2", 1, "\"d3\""),
+        createStubEventWithStreamIdAndData("sid1", 3, "\"d4\""),
+        createStubEventWithStreamIdAndData("sid2", 2, "\"d5\"")
+        ));
+
+
+    assertThat(repo.findAllByStreamId("sid1"))
+      .hasSize(3)
+      .extracting("data")
+      .containsExactly("\"d1\"", "\"d2\"", "\"d4\"");
+
+    assertThat(repo.findAllByStreamId("sid2"))
+      .hasSize(2)
+      .extracting("data")
+      .containsExactly("\"d3\"", "\"d5\"");
+  }
+
+  private EventJpaEntity createStubEventWithStreamId(String streamId, int version) {
+    return createStubEventWithStreamIdAndData(streamId, version, "\"test json\"");
+  }
+
+  private EventJpaEntity createStubEventWithStreamIdAndData(String streamId, int version, String data) {
+    EventJpaEntity event = new EventJpaEntity();
+    long id = Factory.getTsid().toLong();
+    event.setId(id);
+    event.setData(data);
+    Map<String, String> meta = Map.of(
+        "created", LocalDateTime.now().toString(),
+        "ip", "127.0.0.1",
+        "userId", "mario");
+    event.setMeta(meta);
+    event.setEventType("string");
+    event.setEventTypeVersion(1);
+    event.setStreamId(streamId);
+    event.setVersion(version);
+
+    return event;
   }
 
 }
