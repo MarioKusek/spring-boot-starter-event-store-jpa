2219a4413a2beff51137475f92cda8c9241616a1
Mario Kusek <mario.kusek@fer.hr>
2024-05-30 21:06:17 +0200

    Fix autoconfiguration

--- src/main/java/hr/fer/eventstore/jpa/EventStoreAutoConfiguration.java
+++ src/main/java/hr/fer/eventstore/jpa/EventStoreAutoConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -13,6 +14,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 @ConditionalOnBean(DataSource.class)
 @ConditionalOnClass(JpaRepository.class)
 @Import({EventStoreConfiguration.class})
+@EntityScan(basePackageClasses =  {EventStoreAutoConfiguration.class})
 public class EventStoreAutoConfiguration {
 
 }
