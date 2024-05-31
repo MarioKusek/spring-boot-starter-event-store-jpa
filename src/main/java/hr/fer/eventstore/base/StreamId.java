a07a44baeb1c377f491ea41249de598c069a0bf7
Mario Kusek <mario.kusek@fer.hr>
2024-05-31 10:42:34 +0200

    Refactored StreamId to have prefix and randomValue.

--- src/main/java/hr/fer/eventstore/base/StreamId.java
+++ src/main/java/hr/fer/eventstore/base/StreamId.java
@@ -4,39 +4,69 @@ import java.util.Objects;
 
 import io.hypersistence.tsid.TSID;
 
-public interface StreamId {
-  String toValue();
+public final class StreamId {
+  private String prefix;
+  private String randomValue;
 
-  static StreamId of(String value) {
-    return new StreamId() {
-      @Override
-      public String toValue() {
-        return value;
-      }
-
-      @Override
-      public int hashCode() {
-        return Objects.hash(value);
-      }
-
-      @Override
-      public boolean equals(Object obj) {
-        if (this == obj)
-          return true;
-        if (!(obj instanceof StreamId))
-          return false;
-        StreamId other = (StreamId) obj;
-        return Objects.equals(toValue(), other.toValue());
-      }
-
-      @Override
-      public String toString() {
-        return String.format("StreamId[value: %s]", toValue());
-      }
-    };
+  private StreamId(String prefix, String randomValue) {
+    this.prefix = prefix;
+    this.randomValue = randomValue;
   }
 
-  static StreamId ofPrefix(String prefix) {
+  public String prefix() {
+    return prefix;
+
+  }
+
+  public String random() {
+    return randomValue;
+  }
+
+  public String toValue() {
+    if(prefix.isEmpty())
+      return randomValue;
+    else
+      return String.format("%s-%s", prefix, randomValue);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(prefix, randomValue);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (!(obj instanceof StreamId))
+      return false;
+    StreamId other = (StreamId) obj;
+    return Objects.equals(prefix, other.prefix) && Objects.equals(randomValue, other.randomValue);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("StreamId[value: %s]", toValue());
+  }
+
+  public static StreamId of(String value) {
+    int indexOfDivider = value.lastIndexOf("-");
+    if (indexOfDivider == -1)
+      return new StreamId("", value);
+    else
+      return new StreamId(value.substring(0, indexOfDivider),
+          value.substring(indexOfDivider + 1, value.length()));
+  }
+
+  public static StreamId ofPrefix(String prefix) {
     return of(prefix + "-" + TSID.fast().toString());
   }
+
+  public static StreamId of(String prefix, String randomValue) {
+    if(randomValue.contains("-"))
+      throw new IllegalArgumentException("Random value can not have '-'.");
+    return new StreamId(prefix, randomValue);
+  }
+
+
 }
---
+++ src/test/java/hr/fer/eventstore/base/StreamIdTest.java
@@ -0,0 +1,81 @@
+package hr.fer.eventstore.base;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class StreamIdTest {
+
+  @Test
+  void of_withoutPrefix() throws Exception {
+    StreamId streamId = StreamId.of("randomValue");
+
+    assertThat(streamId.toValue()).isEqualTo("randomValue");
+    assertThat(streamId.prefix()).isEmpty();
+    assertThat(streamId.random()).isEqualTo("randomValue");
+  }
+
+  @Test
+  void of_withPrefix() throws Exception {
+    StreamId streamId = StreamId.of("somePrefix-randomValue");
+
+    assertThat(streamId.random()).isEqualTo("randomValue");
+    assertThat(streamId.prefix()).isEqualTo("somePrefix");
+    assertThat(streamId.toValue()).isEqualTo("somePrefix-randomValue");
+  }
+
+  @Test
+  void of_withPrefixThatHasHyphen() throws Exception {
+    StreamId streamId = StreamId.of("some-prefix-randomValue");
+
+    assertThat(streamId.random()).isEqualTo("randomValue");
+    assertThat(streamId.prefix()).isEqualTo("some-prefix");
+    assertThat(streamId.toValue()).isEqualTo("some-prefix-randomValue");
+  }
+
+  @Test
+  void ofPrefix() throws Exception {
+    StreamId streamId = StreamId.ofPrefix("somePrefix");
+
+    assertThat(streamId.toValue()).startsWith("somePrefix-");
+  }
+
+  @Test
+  void ofPrefix_withPrefixWithHyphen() throws Exception {
+    StreamId streamId = StreamId.ofPrefix("some-prefix");
+
+    assertThat(streamId.toValue()).startsWith("some-prefix-");
+  }
+
+  @Test
+  void of_withTwoArguments() throws Exception {
+    StreamId streamId = StreamId.of("some-prefix", "someRandomValue");
+
+    assertThat(streamId.prefix()).isEqualTo("some-prefix");
+    assertThat(streamId.random()).isEqualTo("someRandomValue");
+    assertThat(streamId.toValue()).isEqualTo("some-prefix-someRandomValue");
+  }
+
+  @Test
+  void of_withTwoArgumentsAndRandomValueHasHyphen() throws Exception {
+    assertThrows(IllegalArgumentException.class, () -> {
+      StreamId.of("some-prefix", "some-random-value");
+    });
+  }
+
+  @Test
+  void equalsAndHashCode() throws Exception {
+    StreamId s1 = StreamId.of("p-v");
+    StreamId s2 = StreamId.of("p", "v");
+
+    assertThat(s1.equals(s2)).isTrue();
+    assertThat(s1.hashCode()).isEqualTo(s2.hashCode());
+    assertThat(s1.equals(s1)).isTrue();
+    assertThat(s1.equals("")).isFalse();
+    assertThat(s1.equals(StreamId.of("p1-v"))).isFalse();
+    assertThat(s1.equals(StreamId.of("p-v1"))).isFalse();
+  }
+
+
+}
